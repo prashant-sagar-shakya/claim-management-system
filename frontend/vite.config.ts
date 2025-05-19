7a0cf48543fe7f80312aa70ee16ddc9f5fecf2d7
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2025-05-19 11:14:37 +0530

    Vite Configured

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,27 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
+    define: {
+      "import.meta.env.VITE_API_URL":
+        "https://claim-management-system-6okl.onrender.com",
+    },
+    server: {
+      port: 5173,
+      open: true,
+    },
+    build: {
+      outDir: "dist",
+    },
+  };
 });
