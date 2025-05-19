dc65869025447cfbf6ce3f38b157a8181b14ddb1
prashant-sagar-shakya <prashantshakya43602003@gmail.com>
2025-05-19 11:35:33 +0530

    frontend url set to vercel

--- backend/src/services/auth.service.ts
+++ backend/src/services/auth.service.ts
@@ -255,9 +255,7 @@ export class AuthService {
 
       await user.save({ validateBeforeSave: false });
 
-      const resetURL = `${
-        process.env.FRONTEND_URL || "http://localhost:5173"
-      }/reset-password/${resetToken}`;
+      const resetURL = `${"https://claim-management-system-jade.vercel.app/"}/reset-password/${resetToken}`;
       console.log("--- PASSWORD RESET LINK (FOR TESTING) ---");
       console.log(resetURL);
       console.log("-----------------------------------------");
